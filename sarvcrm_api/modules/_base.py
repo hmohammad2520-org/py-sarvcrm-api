b03e535445b47234f3d5e3b5eda0f7001c996852
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-12-12 19:49:43 +0330

    Doc Improvements

--- sarvcrm_api/modules/_base.py
+++ sarvcrm_api/modules/_base.py
@@ -1,46 +1,81 @@
 from sarvcrm_api.type_hints import SarvGetMethods
 
 class SarvModule:
+    """
+    A base class for interacting with a Sarv CRM module. This class provides methods to 
+    create, read, update, delete, and manage relationships of records in a module.
+    
+    Attributes:
+        _module_name (str): The name of the module.
+        _label_en (str): The label of the module in English.
+        _label_pr (str): The label of the module in Persian.
+        _client (SarvClient): The client instance used to send requests to the Sarv CRM API.
+    """
+    
     _module_name = ''
     _label_en = 'BASE_CLASS'
     _label_pr = 'کلاس اصلی'
 
     def __init__(self, _client):
-        """Initiate the Module class and set the clinet as a local varible"""
+        """
+        Initializes the SarvModule instance with a given client.
+
+        Args:
+            _client (SarvClient): The client used for making API requests to Sarv CRM.
+        """
         from sarvcrm_api import SarvClient
         self._client: SarvClient = _client
 
+    def create_get_parms(self, sarv_get_method: SarvGetMethods, **addition) -> dict:
+        """
+        Constructs the parameters for a 'GET' request based on the provided method and additional parameters.
 
-    def create_get_parms(
-            self,
-            sarv_get_method:SarvGetMethods,
-            **addition
-            ) -> dict:
-        """Create the get parameter with the method and module"""
+        Args:
+            sarv_get_method (SarvGetMethods): The specific 'GET' method to retrieve data.
+            **addition: Additional parameters to customize the request.
 
+        Returns:
+            dict: The parameters to be used in the GET request.
+        """
         return self._client.create_get_parms(sarv_get_method, self, **addition)
 
-
     def create(self, **KWArgs) -> str:
-        """Creates the item in the module"""
+        """
+        Creates a new item in the module with the provided field values.
 
+        Args:
+            **KWArgs: The fields and values to be used in the creation of the item.
+
+        Returns:
+            str: The ID of the newly created item.
+        """
         return self._client.send_request(
             request_method='POST',
             get_parms=self.create_get_parms('Save'),
             post_parms=KWArgs,
         ).get('id', {})
 
-
     def read_list(
             self,
-            query:str = None,
-            order_by:str = None,
-            select_fields:list[str] = None,
-            limit:int = None,
+            query: str = None,
+            order_by: str = None,
+            select_fields: list[str] = None,
+            limit: int = None,
             offset: int = None,
-            ) -> list:
-        """Returns a list of items"""
+    ) -> list:
+        """
+        Retrieves a list of items from the module, optionally filtered by the specified parameters.
 
+        Args:
+            query (str, optional): A query to filter the results.
+            order_by (str, optional): A field to order the results by.
+            select_fields (list[str], optional): A list of fields to include in the response.
+            limit (int, optional): The maximum number of items to retrieve.
+            offset (int, optional): The number of items to skip before starting to return results.
+
+        Returns:
+            list: A list of items from the module.
+        """
         post_parms = {
             'query': query,
             'order_by': order_by,
@@ -56,38 +91,60 @@ class SarvModule:
             post_parms=post_parms,
         ).get('data')
 
+    def read_record(self, pk: str) -> dict:
+        """
+        Retrieves a single item from the module using its unique identifier (ID).
 
-    def read_record(self, pk:str) -> dict:
-        """Returns item from id of the item"""
+        Args:
+            pk (str): The unique identifier (ID) of the item to retrieve.
 
+        Returns:
+            dict: The data of the retrieved item.
+        """
         return self._client.send_request(
             request_method='GET',
             get_parms=self.create_get_parms('Retrieve', id=pk),
         ).get('data')[0]
 
+    def update(self, pk: str, **fields_data) -> str:
+        """
+        Updates an existing item in the module with the given field values.
 
-    def update(self, pk:str, **fields_data) -> str:
-        """Updates the fields from id of the item"""
+        Args:
+            pk (str): The unique identifier (ID) of the item to update.
+            **fields_data: The fields and values to update in the item.
 
+        Returns:
+            str: The ID of the updated item.
+        """
         return self._client.send_request(
-            request_method = 'PUT',
-            get_parms = self.create_get_parms('Save', id=pk),
-            post_parms = fields_data,
+            request_method='PUT',
+            get_parms=self.create_get_parms('Save', id=pk),
+            post_parms=fields_data,
         ).get('id')
 
+    def delete(self, pk: str) -> str | None:
+        """
+        Deletes an item from the module using its unique identifier (ID).
 
-    def delete(self, pk:str) -> str | None:
-        """Deletes the item with id"""
+        Args:
+            pk (str): The unique identifier (ID) of the item to delete.
 
+        Returns:
+            str | None: The ID of the deleted item or None if no item was deleted.
+        """
         return self._client.send_request(
-            request_method = 'DELETE',
-            get_parms = self.create_get_parms('Save', id=pk),
+            request_method='DELETE',
+            get_parms=self.create_get_parms('Save', id=pk),
         ).get('id')
-
 
     def get_module_fields(self) -> dict[str, dict]:
-        """Gets all of the fields of a module"""
+        """
+        Retrieves the list of all fields for the module.
 
+        Returns:
+            dict: A dictionary containing all the fields of the module.
+        """
         return self._client.send_request(
             request_method='GET',
             get_parms=self.create_get_parms('GetModuleFields'),
@@ -95,15 +152,27 @@ class SarvModule:
 
     def get_relationships(
             self,
-            related_field:str,
-            query:str = None,
-            order_by:str = None,
-            select_fields:list[str] = None,
-            limit:int = None,
+            related_field: str,
+            query: str = None,
+            order_by: str = None,
+            select_fields: list[str] = None,
+            limit: int = None,
             offset: int = None,
-            ) -> list:
-        """returns list of items in relationship of the related field"""
+    ) -> list:
+        """
+        Retrieves a list of related items for a specific field in the module.
 
+        Args:
+            related_field (str): The related field to fetch relationships for.
+            query (str, optional): A query to filter the results.
+            order_by (str, optional): A field to order the results by.
+            select_fields (list[str], optional): A list of fields to include in the response.
+            limit (int, optional): The maximum number of items to retrieve.
+            offset (int, optional): The number of items to skip before starting to return results.
+
+        Returns:
+            list: A list of related items.
+        """
         post_parms = {
             'query': query,
             'order_by': order_by,
@@ -119,15 +188,23 @@ class SarvModule:
             post_parms=post_parms,
         )
 
-
     def save_relationships(
             self,
-            pk:str,
-            field_name:str,
-            related_records:list,
-            ) -> list:
-        """Saves the relationship"""
+            pk: str,
+            field_name: str,
+            related_records: list,
+    ) -> list:
+        """
+        Saves a relationship between the current item and related records.
 
+        Args:
+            pk (str): The unique identifier (ID) of the item.
+            field_name (str): The name of the field to establish the relationship.
+            related_records (list): A list of related records to associate with the item.
+
+        Returns:
+            list: A list of the related records saved in the relationship.
+        """
         return self._client.send_request(
             request_method='POST',
             get_parms=self.create_get_parms('SaveRelationships', id=pk),
@@ -135,7 +212,19 @@ class SarvModule:
         )
 
     def __repr__(self) -> str:
+        """
+        Returns a string representation of the SarvModule instance.
+
+        Returns:
+            str: A string representation of the SarvModule instance.
+        """
         return f'{self.__class__.__name__}(client: SarvClient)'
 
     def __str__(self) -> str:
+        """
+        Returns a string that describes the SarvModule instance.
+
+        Returns:
+            str: A description of the SarvModule instance.
+        """
         return f'<SarvModule {self._label_en}>'
--- sarvcrm_api/sarv_client.py
+++ sarvcrm_api/sarv_client.py
@@ -16,6 +16,11 @@ requests_method_map: dict[SarvLanguageType, Callable] = {
 }
 
 class SarvClient(ModulesMixin):
+    """
+    SarvClient provides methods for interacting with the SarvCRM API. 
+    It supports authentication, data retrieval, and other API functionalities.
+    """
+
     def __init__(
             self, 
             base_url: str,
@@ -26,7 +31,18 @@ class SarvClient(ModulesMixin):
             language: SarvLanguageType = 'en_US',
             is_password_md5: bool = False,
             ) -> None:
-        """Initialize the SarvClient."""
+        """
+        Initialize the SarvClient.
+
+        Args:
+            base_url (str): The base URL for the SarvCRM API.
+            utype (str): The user type for authentication.
+            username (str): The username for authentication.
+            password (str): The password for authentication.
+            login_type (Optional[str]): The login type for authentication.
+            language (SarvLanguageType): The language to use, default is 'en_US'.
+            is_password_md5 (bool): Whether the password is already hashed using MD5.
+        """
 
         self.base_url = base_url
         self.utype = utype
@@ -50,8 +66,17 @@ class SarvClient(ModulesMixin):
             sarv_module: Optional[SarvModule | str] = None,
             **addition
             ) -> dict:
-        """Create the get parameter with the method and module"""
+        """
+        Create the GET parameters with the method and module.
 
+        Args:
+            sarv_get_method (SarvGetMethods): The API method to call.
+            sarv_module (Optional[SarvModule | str]): The module name or object.
+            addition: Additional parameters to include in the GET request.
+
+        Returns:
+            dict: The constructed GET parameters.
+        """
         module_name = None
 
         if sarv_module is not None:
@@ -76,17 +101,19 @@ class SarvClient(ModulesMixin):
 
     def iso_time_output(output_method: TimeOutput, dt: datetime | timedelta) -> str:
         """
-        Takes a datetime or timedelta object and returns a string based on output method.
-        these results are acceptable by sarvcrm api and should use this as time formats.
+        Generate a formatted string from a datetime or timedelta object.
+
+        These formats are compliant with the SarvCRM API time standards.
+
         Args:
-            output_method: Determines the output format. Can be 'date', 'datetime', or 'time'.
-            dt: A datetime or timedelta object.
+            output_method (TimeOutput): Determines the output format ('date', 'datetime', or 'time').
+            dt (datetime | timedelta): A datetime or timedelta object.
 
         Returns:
-            A string representing the date, datetime, or time.
-            - date: "YYYY-MM-DD"
-            - datetime: "YYYY-MM-DDTHH:MM:SS+HH:MM"
-            - time: "HH:MM:SS"
+            str: A string representing the date, datetime, or time.
+                - date: "YYYY-MM-DD"
+                - datetime: "YYYY-MM-DDTHH:MM:SS+HH:MM"
+                - time: "HH:MM:SS"
         """
         if isinstance(dt, timedelta):
             dt = datetime.now(timezone.utc) + dt
@@ -111,8 +138,21 @@ class SarvClient(ModulesMixin):
             get_parms: dict = None,
             post_parms: dict = None,
             ) -> dict:
-        """Send a request to the Sarv API and returns the data parameter of the response"""
+        """
+        Send a request to the Sarv API and return the response data.
 
+        Args:
+            request_method (RequestMethod): The HTTP method for the request ('GET', 'POST', etc.).
+            head_parms (dict): The headers for the request.
+            get_parms (dict): The GET parameters for the request.
+            post_parms (dict): The POST parameters for the request.
+
+        Returns:
+            dict: The data parameter from the server response.
+
+        Raises:
+            SarvException: If the server returns an error response.
+        """
         requests_function = requests_method_map.get(request_method, None)
 
         head_parms = head_parms or {}
@@ -163,8 +203,12 @@ class SarvClient(ModulesMixin):
 
 
     def login(self) -> str:
-        """Authenticate the user and retrieve a token."""
+        """
+        Authenticate the user and retrieve an access token.
 
+        Returns:
+            str: The access token for authenticated requests.
+        """
         post_parms = {
             'utype': self.utype,
             'user_name': self.username,
@@ -187,7 +231,11 @@ class SarvClient(ModulesMixin):
     
 
     def logout(self) -> None:
-        """Clears token from the instance"""
+        """
+        Clears the access token from the instance.
+
+        This method should be called to invalidate the session.
+        """
         if self.token:
             self.token = ''
 
@@ -197,8 +245,16 @@ class SarvClient(ModulesMixin):
             number:str,
             module: Optional[SarvModule | str] = None
             ) -> None:
-        """Searches the crm by phone number and retrives the module item"""
+        """
+        Search the CRM by phone number and retrieve the module item.
 
+        Args:
+            number (str): The phone number to search for.
+            module (Optional[SarvModule | str]): The module to search in.
+
+        Returns:
+            dict: The data related to the phone number if found.
+        """
         return self.send_request(
             request_method='GET',
             get_parms=self.create_get_parms('SearchByNumber', sarv_module=module, number=number),
@@ -206,8 +262,20 @@ class SarvClient(ModulesMixin):
 
 
     def __repr__(self):
+        """
+        Provides a string representation for debugging purposes.
+
+        Returns:
+            str: A string containing the class name and key attributes.
+        """
         return f'{self.__class__.__name__}(utype={self.utype}, username={self.username})'
 
 
     def __str__(self) -> str:
+        """
+        Provides a human-readable string representation of the instance.
+
+        Returns:
+            str: A simplified string representation of the instance.
+        """
         return f'<SarvClient {self.utype}>'
