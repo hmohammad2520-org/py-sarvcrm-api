07b52af28bf37647b346276d97454424b8929582
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-12-14 19:53:26 +0330

    Update type Alias

--- sarvcrm_api/fields/_types.py
+++ sarvcrm_api/fields/_types.py
@@ -5,156 +5,152 @@ from datetime import date, datetime
 
 from ._base import SarvFieldType
 
-class Linkable(SarvFieldType):
-    _field_type_name: str = 'Linkable'
-    _alias: TypeAlias = None
-
 
 class AssignedUserName(SarvFieldType):
     _field_type_name: str = 'assigned_user_name'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Base(SarvFieldType):
     _field_type_name: str = 'base'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = object
 
 
 class Bool(SarvFieldType):
     _field_type_name: str = 'bool'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = bool
 
 
 class Boolean(SarvFieldType):
     _field_type_name: str = 'boolean'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = bool
 
 
 class Char(SarvFieldType):
     _field_type_name: str = 'char'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Currency(SarvFieldType):
     _field_type_name: str = 'currency'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = Decimal
 
 
 class Date(SarvFieldType):
     _field_type_name: str = 'date'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = date
 
 
 class Datetime(SarvFieldType):
     _field_type_name: str = 'datetime'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = datetime
 
 
 class Datetimecombo(SarvFieldType):
     _field_type_name: str = 'datetimecombo'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = datetime
 
 
 class Decimal(SarvFieldType):
     _field_type_name: str = 'decimal'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = Decimal
 
 
 class Durationcombo(SarvFieldType):
     _field_type_name: str = 'durationcombo'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Email(SarvFieldType):
     _field_type_name: str = 'email'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Enum(SarvFieldType):
     _field_type_name: str = 'enum'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class File(SarvFieldType):
     _field_type_name: str = 'file'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Fullname(SarvFieldType):
     _field_type_name: str = 'fullname'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Function(SarvFieldType):
     _field_type_name: str = 'function'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = callable
 
 
 class Id(SarvFieldType):
     _field_type_name: str = 'id'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = Union[UUID, str]
 
 
 class Int(SarvFieldType):
     _field_type_name: str = 'int'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = int
 
 
 class Integer(SarvFieldType):
     _field_type_name: str = 'integer'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = int
 
 
 class Linkable(SarvFieldType):
     _field_type_name: str = 'linkable'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Multienum(SarvFieldType):
     _field_type_name: str = 'multienum'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = List[str]
 
 
 class Name(SarvFieldType):
     _field_type_name: str = 'name'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Parent(SarvFieldType):
     _field_type_name: str = 'parent'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = Union[UUID, str]
 
 
 class ParentType(SarvFieldType):
     _field_type_name: str = 'parent_type'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Phone(SarvFieldType):
     _field_type_name: str = 'phone'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Relate(SarvFieldType):
     _field_type_name: str = 'relate'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = Union[str, UUID]
 
 
 class Text(SarvFieldType):
     _field_type_name: str = 'text'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
 
 
 class Tinyinit(SarvFieldType):
     _field_type_name: str = 'tinyinit'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = bool
 
 
 class Tree(SarvFieldType):
     _field_type_name: str = 'tree'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = object
 
 
 class Varchar(SarvFieldType):
     _field_type_name: str = 'varchar'
-    _alias: TypeAlias = None
+    _alias: TypeAlias = str
