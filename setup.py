a60ec2d5c5a0bea0cff303c7ef2e2f4eb4c87c2e
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-12-16 13:05:29 +0330

    Improved toml file and edited version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='py-sarvcrm-api',
-    version='1.0.0',
+    version='0.1.0',
     description='simple sarvcrm api module',
     author='Radin System',
     author_email='info@rsto.ir',
