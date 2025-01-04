ffdb96d638dad31ac97aa8d3fee29502ade31b8f
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-01-04 16:31:32 +0330

    fixed Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='py-sarvcrm-api',
-    version='0.1.1',
+    version='0.1.2',
     license="MIT",
     description='simple sarvcrm api module',
     author='Radin-System',
