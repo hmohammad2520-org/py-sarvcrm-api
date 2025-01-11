867a023fb4b400c47797f6861c5bdfc628c761ef
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-01-11 11:32:49 +0330

    Added new varibles

--- sarvcrm_api/sarv_url.py
+++ sarvcrm_api/sarv_url.py
@@ -1,5 +1,12 @@
-SarvURL: str = 'https://app.sarvcrm.com/API.php'
+from typing import LiteralString
+
+SarvAPI_v5: LiteralString = 'https://app.sarvcrm.com/API.php'
+SarvFrontend: LiteralString = 'https://app.sarvcrm.com/index.php'
+
+SarvURL = SarvAPI_v5
 
 __all__ = [
-    'SarvURL'
+    'SarvAPI_v5',
+    'SarvFrontend',
+    'SarvURL',
 ]
