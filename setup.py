044b3a1464c900f33f0f4b7717ca2f073ae05da5
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-01-21 15:55:38 +0330

    Update Version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='py-sarvcrm-api',
-    version='1.0.0',
+    version='1.0.1',
     license="MIT",
     description='simple sarvcrm api module',
     author='Radin-System',
