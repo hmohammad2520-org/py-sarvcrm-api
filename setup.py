7a9ee92a561e1ca361489f6a6c04f9da8367a217
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-07-26 10:45:20 +0330

    Bumped classmods

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         'requests==2.32.4',
         'requests-cache==1.2.1',
-        'classmods==0.3.2'
+        'classmods==0.3.3'
     ],
     packages=find_packages(exclude=['test', 'test.*']),
     include_package_data=True,
